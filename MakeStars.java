525051ec31f1356b3acfec1b39619538ac7011d0
Yuqing Wu <ywu20@stuy.edu>
2021-01-04 13:08:16 -0500

    works perhaps

--- MakeStars.java
+++ MakeStars.java
@@ -4,7 +4,15 @@ public class MakeStars{
     Scanner n=new Scanner(System.in);
     while(n.hasNextLine()){
       String x = n.nextLine();
-      System.out.println(x);
+      Scanner n2=new Scanner(x);
+      while (n2.hasNext()){
+        String xx=n2.next();
+        for(int i=0;i<xx.length();i++){
+          System.out.print('*');
+        }
+        System.out.print(' ');
+      }
+      System.out.println();
     }
   }
 }
