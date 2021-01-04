8fc0ac1b73a10cc7bd1f8b8437ba9e530dfe8ef0
Yuqing Wu <ywu20@stuy.edu>
2021-01-04 13:17:24 -0500

    check if first character is alphabet

--- PigLatin.java
+++ PigLatin.java
@@ -1,3 +1,22 @@
+import java.util.*;
 public class PigLatin{
-  
+  public static boolean isAlpha(char c){
+    char[] alpha = {'a','e','i','o','u'};
+    for(int i=0;i<alpha.length;i++){
+      if(c==alpha[i]){
+        return true;
+      }
+    }
+    return false;
+  }
+  public static String pigLatinSimple(String s){
+    if(isAlpha(s.charAt(0))){
+      System.out.println("true");
+    }
+    return "hi";
+  }
+
+  public static void main(String[] args){
+    pigLatinSimple(args[0]);
+  }
 }
